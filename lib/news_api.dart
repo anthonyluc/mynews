fabc93c921a7204047b4ce9f3ebab9000192b6d9
anthonyluc <anthony.luc69@gmail.com>
2024-05-04 16:13:37 +0200

    Listview builder of api response

--- lib/news_api.dart
+++ lib/news_api.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 const newsUrl =
-    'https://newsapi.org/v2/everything?q=tesla&from=2024-04-04&sortBy=publishedAt&apiKey=c7d986faea5b4c9b8e2d65bf812cc792';
+    'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c7d986faea5b4c9b8e2d65bf812cc792';
 
 class NewsApi {
   static Future getArticles() {
