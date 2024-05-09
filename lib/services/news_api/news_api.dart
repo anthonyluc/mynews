16613ce100c2b11c4b1a56311af4cf6d66625e80
anthonyluc <anthony.luc69@gmail.com>
2024-05-09 11:56:34 +0200

    firebase function test & api key in gitignore

--- lib/services/news_api/news_api.dart
+++ lib/services/news_api/news_api.dart
@@ -1,9 +1,10 @@
 import 'package:http/http.dart' as http;
+import '../../api_key.dart';
 
 class NewsApi {
   static Future getArticles(String category) async {
     final newsUrl =
-        'https://newsapi.org/v2/everything?q=$category&language=fr&sortBy=publishedAt&apiKey=c7d986faea5b4c9b8e2d65bf812cc792';
+        'https://newsapi.org/v2/everything?q=$category&language=fr&sortBy=publishedAt&apiKey=$newsApiKey';
     return await http.get(Uri.parse(newsUrl));
   }
 }
